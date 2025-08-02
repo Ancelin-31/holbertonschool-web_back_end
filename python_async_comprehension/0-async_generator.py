0a1d85e2cac04d9ccb0defe4f80d785628df381e
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-02 19:30:43 +0200

    fix syntax

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 """
-create a rancom loop 10 times
+create a random loop 10 times
 """
-from typing import AsyncGenerator, List
+from typing import AsyncGenerator
 import asyncio
 import random
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    create a random loop 10 times
+    Coroutine that loops 10 times, waits 1 second, and yields a random float between 0 and 10.
     """
-    i = float
     for _ in range(10):
         await asyncio.sleep(1)
-        yield i
+        yield random.uniform(0, 10)
