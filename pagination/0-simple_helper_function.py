f75eb5425ed96ef1573a5cb37b793bfe2eae9d11
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-11 10:58:09 +0200

    pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-# This module provides a helper function for pagination calculations.
+"""This module provides a helper function for pagination calculations."""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple:
-    # Returns a tuple of (start_index, end_index) for pagination.
-    # Page numbers are 1-indexed.
+    """Returns a tuple of (start_index, end_index) for pagination.
+    Page numbers are 1-indexed."""
 
     start_index: int = (page - 1) * page_size
     end_index: int = page * page_size
