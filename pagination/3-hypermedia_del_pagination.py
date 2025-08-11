3b287c85274e727c66c150ea2cb29ee18f808481
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-11 14:52:43 +0200

    pycodestyle

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Dataset indexed by sorting position, starting at 0, as 
+        """Dataset indexed by sorting position, starting at 0, as
         """
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
