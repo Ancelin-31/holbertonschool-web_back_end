e4fa1f5872c4c43901e3eb725b6d3175cdf55636
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-11 10:50:56 +0200

    Forces varaiable type

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,6 @@ def index_range(page: int, page_size: int) -> tuple:
     # Returns a tuple of (start_index, end_index) for pagination.
     # Page numbers are 1-indexed.
 
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
+    start_index: int = (page - 1) * page_size
+    end_index: int = page * page_size
     return (start_index, end_index)
