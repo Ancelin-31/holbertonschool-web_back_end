480772e09e2ed5bab088c1ba59cc913ab7b40f1f
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-14 15:25:57 +0200

    Adds a python function to list all documents in a NoSQL collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+def list_all(mongo_collection):
+    """
+    Lists all documents in a MongoDB collection.
+    
+    Args:
+        mongo_collection: pymongo collection object.
+    
+    Returns:
+        list: A list of all documents in the collection.
+              Returns an empty list if there are no documents.
+    """
+    if mongo_collection is None:
+        return []
+    
+    return list(mongo_collection.find())
