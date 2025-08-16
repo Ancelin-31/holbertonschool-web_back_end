1b4ead4c625847a6f6758644498c690481b8e470
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-16 17:24:05 +0200

    Adds a function that updates all topics of a document

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Updates all topics of a document
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates all topics of a document
+    """
+    return mongo_collection.update_many(
+        { "name": name },
+        { "$set": { "topics": topics } }
+    )
