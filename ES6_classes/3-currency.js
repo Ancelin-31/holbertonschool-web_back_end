4e178b4fa9b5098c94219fc7f7121d79d0f4d87f
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-20 21:16:31 +0200

    Sets a getter and a setter for attributes of currency class

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,43 @@
+export default class Currency {
+  constructor(code, name) {
+    // Validate types
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._code = code;
+    this._name = name;
+  }
+
+  // ---- code ----
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  // ---- name ----
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // ---- displayFullCurrency ----
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
