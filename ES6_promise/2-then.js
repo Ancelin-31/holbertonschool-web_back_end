8b45e7fa4131f05f16aa3ebfa409f332ecc4565c
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-08-25 16:11:08 +0200

    Handles an API response and logs a message whatever the resolution

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  const response = promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+
+    return response;
+}
